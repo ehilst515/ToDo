cdb5ffd4c92984c64604cda8cdf60bf6fb59095f
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-12 22:35:13 -0500

    Added form to Home component

--- lab-36/src/components/Home.js
+++ lab-36/src/components/Home.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
 export default function () {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <form>
+                <div class="form-group">
+                    <label>Add a Task</label>
+                    <div class="d-inline-flex p-4">
+                        <input type="text" class="form-control" placeholder="Enter task"></input>
+                    </div>
+                    <small id="formHelp" class="form-text text-muted">Enter a task to add to the list.</small>
+                </div>
+                <Button>Submit</Button>
+            </form>
+        </div>
+
     );
 }
