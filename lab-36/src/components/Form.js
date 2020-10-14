0cb4b1f988b4c2c04999e6fe9cd37cb3f73d7cc4
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-14 14:31:59 -0500

    Added save and delete task function and logic

--- lab-36/src/components/Form.js
+++ lab-36/src/components/Form.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Form from 'react-bootstrap/Form'
 
+export default function TaskForm(props) {
+    const { onSave } = props;
 
-export default function () {
+    function handleSubmit(e){
+        e.preventDefault();
+        const {details} = e.target;
+        const {assignee} = e.target;
+        const {date} = e.target;
+        const {difficulty} = e.target;
+
+        const task = {
+            details: details.value,
+            assignee: assignee.value,
+            date: date.value,
+            difficulty: difficulty.value,
+        };
+
+        console.log(task);
+        onSave(task);
+    };
+
     return (
-        <form>
-            <h3>Add a Task</h3>
-            <div class="form-group col">
-                <label for="details">Details</label>
-                <input class="form-control" placeholder="Task Details" id="details"></input>
-                <label for="assignee">Assigned To</label>
-                <input class="form-control" placeholder="Assignee Name" id="assignee"></input>
-                <label for="due-date">Due Date</label>
-                <input type="date" class="form-control" placeholder="Date Task is Due" id="due-date"></input>
-                <label for="formControlRange">Difficulty</label>
-                <input type="range" class="form-control-range" id="formControlRange" min="1" max="5"></input>
-                <small id="formHelp" class="form-text text-muted">Submit a task to add to the list.</small>
-            </div>
-            <Button>Submit</Button>
-        </form>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group>
+                <h2>Add a Task</h2>
+                <div>
+                    <Form.Label>Details</Form.Label>
+                    <Form.Control placeholder="Task Details" name="details"></Form.Control>
+                    <Form.Label>Assigned To</Form.Label>
+                    <Form.Control placeholder="Assignee Name" name="assignee"></Form.Control>
+                    <Form.Label>Due Date</Form.Label>
+                    <Form.Control type="date" className="form-control" placeholder="Date Task is Due" name="date"></Form.Control>
+                    <Form.Label>Difficulty</Form.Label>
+                    <Form.Control type="range" className="form-control-range" min="1" max="5" name="difficulty"></Form.Control>
+                    <Form.Text id="formHelp" className="text-muted">Submit a task to add to the list.</Form.Text>
+                </div>
+                <Button type="submit">Submit</Button>
+            </Form.Group>
+        </Form>
     );
 }
